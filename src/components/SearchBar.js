cbc5dc7436f42a625419ecd808018d66f3a2694b
Spencer Tassone <spencerwilliamtassone@gmail.com>
2019-10-17 15:20:36 -0400

    using handle change and handle submit without binding

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,16 +3,27 @@ import React from "react";
 import { Paper, TextField } from "@material-ui/core"
 
 class SearchBar extends React.Component {
+
     state = {
         searchTerm: ""
     };
 
+    handleChange = (event) => {
+        this.setState({
+            searchTerm: event.taget.value
+        })
+    }
+
+    handleSubmit = () => {
+        const { searchTerm } = this.state;
+        
+    }
+    
     render(){
         return(
             <Paper elevation={6} style= {{padding: "25px"}}>
-                <form>
-                    <TextField fullWidth label="Search...">
-                    </TextField>
+                <form onSubmit={this.handleSubmit}>
+                    <TextField fullWidth label="Search..." onChange={this.handleChange} />
                 </form>
             </Paper>
         )
