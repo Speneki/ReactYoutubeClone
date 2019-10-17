a296910eb8995f964dce8abecdf46eaeb4464d8e
Spencer Tassone <spencerwilliamtassone@gmail.com>
2019-10-17 15:11:27 -0400

    clean import on App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import React from "react";
 
+import { Grid } from "@material-ui/core";
+
+import { SearchBar, VideoDetail } from "./components"
+
+import youtube from "./api/youtube"
+
 class App extends React.Component {
     render(){
         return(
-            <h1>Clone</h1>
+            <Grid justify="center" container spacing={16}>
+                <Grid item xs={12}>
+                    <Grid container spacing={16}>
+                        <Grid item xs={12}>
+                            <SearchBar/>
+                        </Grid>
+                        <Grid item xs={8}>
+                            <VideoDetail/>
+                        </Grid>
+                        <Grid item xs={4}>
+                            {/* Video List */}
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </Grid>
         )
     }
 }
---
+++ src/components/SearchBar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class SearchBar extends React.Component {
+    state = {
+        searchTerm: ""
+    };
+
+    render(){
+        return(
+            <h1>Search Bar</h1>
+        )
+    };
+}
+
+export default SearchBar;
