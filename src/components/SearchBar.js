365b5b31c50696140a52da979348a7711adb9fcf
Spencer Tassone <spencerwilliamtassone@gmail.com>
2019-10-17 15:14:05 -0400

    search bar styling

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import { Paper, TextField } from "@material-ui/core"
+
 class SearchBar extends React.Component {
     state = {
         searchTerm: ""
@@ -7,7 +9,12 @@ class SearchBar extends React.Component {
 
     render(){
         return(
-            <h1>Search Bar</h1>
+            <Paper elevation={6} style= {{padding: "25px"}}>
+                <form>
+                    <TextField fullWidth label="Search...">
+                    </TextField>
+                </form>
+            </Paper>
         )
     };
 }
